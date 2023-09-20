3cb1f63d503f846efb0584f263c70b278669f7dd
David Kuehn <dmkuehn6@gmail.com>
2023-09-20 17:55:24 -0400

    Rewrote email sending using async/await to fix issues with Vercel serverless functions

--- app/api/estimate/route.ts
+++ app/api/estimate/route.ts
@@ -1,5 +1,5 @@
+var nodemailer = require("nodemailer");
 import { NextResponse } from "next/server";
-import { EmailDetails, sendMail } from "../../../services/nodemailerService";
 
 export interface EstimateRequest extends Request {
   estimateDetails: EstimateDetails
@@ -14,16 +14,50 @@ export interface EstimateDetails {
 
 export async function POST(request: Request) {
   const requestBodyJson: EstimateDetails = await request.json();
-  console.log(requestBodyJson);
 
-  const messageDetails: EmailDetails = {
-    fromName: requestBodyJson.customerName,
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (error) {
+          console.log(error);
+          reject(error);
+      } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+      }
+    });
+  });
+
+  var mailOptions = {
+    from: {
+      name: requestBodyJson.customerName,
+      address: process.env.NODEMAILER_EMAIL
+    },
     to: "dmkuehn6@gmail.com",
     replyTo: requestBodyJson.customerEmail,
     subject: `New Quote Request from ${requestBodyJson.customerName}`,
-    contents: `A new Quote Request was received from ${requestBodyJson.customerName}, reachable at ${requestBodyJson.customerEmail}.`
-  }
-  await sendMail(messageDetails);
-  console.log("Should have sent message...");
+    text: `A new Quote Request was received from ${requestBodyJson.customerName}, reachable at ${requestBodyJson.customerEmail}.`,
+  };
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err: any, info: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+
   return NextResponse.json({ "testResponse": "Hello World!" });
 }
